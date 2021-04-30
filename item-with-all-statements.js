0503cad7bc619e17d42015df2fc3a74457a0f29f
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2021-04-30 13:50:25 +0200

    Make item-with-all-statements.js create string and time statement

--- item-with-all-statements.js
+++ item-with-all-statements.js
@@ -1,45 +1,69 @@
-import EntityCreationError from './src/EntityCreationError.js';
+async function makeEntitySerialization( apiClient, statements ) {
+    return {
+        labels: {
+            en: {
+                language: 'en', value: 'statement showcase item',
+            },
+        },
+        descriptions: {
+            en: {
+                language: 'en', value: 'item that has statements for every datatype',
+            },
+        },
+        aliases: {
+            en: [
+                {
+                    language: 'en', value: 'goats',
+                },
+                {
+                    language: 'en', value: 'potato',
+                },
+            ],
+        },
+        claims: await Promise.all( statements.map( async ( { datatype, value } ) => {
+            const propertyId = await apiClient.findOrCreatePropertyByDataType( datatype );
+            if( propertyId ) {
+                return {
+                    mainsnak: {
+                        snaktype: 'value',
+                        property: propertyId,
+                        datavalue: value,
+                        datatype: datatype,
+                    },
+                    type: 'statement',
+                    rank: 'normal',
+                };
+            } else {
+                console.log( 'Skipped creating a statement for datatype ' + datatype );
+                return null;
+            }
+        } ).filter( s => s ) ),
+    };
+}
 
 export default async function( apiClient ) {
-
-    const itemId = await apiClient.createItem( 
-        {
-            labels: { 
-                en: { 
-                    language: 'en', value: 'statement showcase item' 
-                } 
-            },
-            descriptions: {
-                en: { 
-                    language: 'en', value: 'item that has statements for every datatype' 
-                } 
-            },
-            aliases: {
-                en: [
-                    { 
-                        language: 'en', value: 'goats' 
-                    },
-                    { 
-                        language: 'en', value: 'potato' 
-                    }
-                ]
-            },
-            claims: [
-                    {
-                        mainsnak: {
-                            snaktype: "value",
-                            property: "P9",
-                            datavalue: {
-                                value: "something",
-                                type: "string"
-                            },
-                            datatype: "string"
-                        },
-                        type: "statement",
-                        rank: "normal"
-                    }
-            ]
-        } 
-    );
+    const stringValue = {
+            value: 'something',
+            type: 'string'
+        };
+    const stringDatatype = 'string';
+    const timeValue = {
+        value: {
+            time: '+2021-04-30T00:00:00Z',
+            timezone: 0,
+            before: 0,
+            after: 0,
+            precision: 11,
+            calendarmodel: 'http://www.wikidata.org/entity/Q1985727',
+        },
+        type: 'time',
+    };
+    const timeDatatype = 'time';
+    const statements = [
+        { datatype: stringDatatype, value: stringValue },
+        { datatype: timeDatatype, value: timeValue },
+    ];
+    const entitySerialization = await makeEntitySerialization( apiClient, statements );
+    const itemId = await apiClient.createItem( entitySerialization );
     console.log( itemId );
 }
